3f7924d734bd57c213cfe9359c3a343ac3d7ac0b
Andre Faria <andre.faria.24@gmail.com>
2024-11-21 18:24:02 -0500

    added aws creds

--- tests/setup/variables.tf
+++ tests/setup/variables.tf
@@ -95,11 +95,16 @@ variable "create_private_subnets" {
   default     = true
 }
 
-variable "AWS_ACCESS_KEY_ID" {
+variable "aws_access_key" {
   type        = string
+  sensitive = true
+}
+variable "aws_secret_key" {
+  type        = string
+  sensitive = true
 }
 
-variable "AWS_SECRET_ACCESS_KEY" {
+variable "aws_token" {
   type        = string
   sensitive = true
 }
